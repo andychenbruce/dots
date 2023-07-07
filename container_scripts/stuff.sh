50deb571dbedcf5d60b3e0b337dd03dde51ee054
陈功 <chengong456@qq.com>
2023-07-07 10:47:56 +0800

    manifest stuff

--- container_scripts/stuff.sh
+++ container_scripts/stuff.sh
@@ -3,16 +3,7 @@
 guix shell --network --container --emulate-fhs \
 	--preserve='^TERM$' \
 	--no-cwd \
-	nss-certs \
-	bash coreutils curl grep sed gawk make cmake findutils \
-	libgccjit gmp openssl \
-	gcc-toolchain:static gcc-toolchain glibc-locales pkg-config clang \
-	ncurses ncurses-with-tinfo \
-	neovim tmux openssl:static \
-	sqlite \
-	valgrind gdb \
-	openssh git \
-	util-linux coreutils htop wget procps less \
-	tar gzip xz \
-	--share=$HOME/containers/tmphome=$HOME -- bash
+	--share=$HOME/containers/tmphome=$HOME \
+	--manifest=manifest.scm \
+	-- bash
 
