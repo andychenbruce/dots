a84aecc70496fe52de45ab96df4161eea6871081
陈功 <chengong456@qq.com>
2024-03-06 20:11:10 -0800

    asdf

--- desktop/start_sway.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-dbus-run-session sway 
-
-killall -w wireplumber
-killall -w pipewire-pulse
-killall -w pipewire
--- guix/home_config/my_configs/fish/fish.fish
+++ guix/home_config/my_configs/fish/fish.fish
@@ -1,34 +1,42 @@
 set --prepend fish_function_path "$HOME/.guix-home/profile/share/fish/functions"
 
 if status is-login
-   fenv source /run/current-system/profile/etc/profile
+  fenv source /run/current-system/profile/etc/profile
 
-   set --export HOME_ENVIRONMENT $HOME/.guix-home
-   $HOME_ENVIRONMENT/on-first-login
-   fenv source $HOME_ENVIRONMENT/setup-environment
+  set --export HOME_ENVIRONMENT $HOME/.guix-home
+  $HOME_ENVIRONMENT/on-first-login
+  fenv source $HOME_ENVIRONMENT/setup-environment
 
-   fenv source $HOME/.config/guix/current/etc/profile
+  fenv source $HOME/.config/guix/current/etc/profile
 
-   set --path --prepend PATH "/run/setuid-programs"
+  set --path --prepend PATH "/run/setuid-programs"
 
-   eval $(ssh-agent -c)
-   trap "ssh-agent -k" exit
+  eval $(ssh-agent -c)
+  trap "ssh-agent -k" exit
 
-   set --export RUSTUP_HOME "$XDG_DATA_HOME"/rustup
-   set --export CARGO_HOME "$XDG_DATA_HOME"/cargo
-   fenv source $CARGO_HOME/env
+  set --export RUSTUP_HOME "$XDG_DATA_HOME"/rustup
+  set --export CARGO_HOME "$XDG_DATA_HOME"/cargo
+  fenv source $CARGO_HOME/env
 
-   set --path --prepend PATH "$HOME/.local/bin"
+  set --path --prepend PATH "$HOME/.local/bin"
+  set --export MPD_HOST "/tmp/mpd.sock"
 
-   set --export MPD_HOST "/tmp/mpd.sock"
+  function start_sway
+    dbus-run-session sway 
+
+    killall -w wireplumber
+    killall -w pipewire-pulse
+    killall -w pipewire
+  end
+
 end
 
 function enter
-    guix shell --check --manifest=$argv[1]
+  guix shell --check --manifest=$argv[1]
 end
 
 if set --query GUIX_ENVIRONMENT
-    set --export LD_LIBRARY_PATH $LIBRARY_PATH
+  set --export LD_LIBRARY_PATH $LIBRARY_PATH
 end
 
 if set --query INSIDE_EMACS
